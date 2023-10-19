256923b1ac8153dc276c6cee230da86fd03f5ae7
Priya Ranjan Kumar <116024984+ranjanrana1712@users.noreply.github.com>
2023-10-19 18:57:10 +0530

    update:html making

---
+++ style.js
@@ -0,0 +1,8 @@
+function reverseStr(str) {
+    var listOfChar = str.split('');
+    var reverseListOfChars = listOfChar.reverse('');
+    var reversedStr = reverseListOfChars.join('');
+    return reverseStr;
+    
+}
+
