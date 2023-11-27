7f52ed27f1e8bf9faa32948b1e59e0b3a69536a8
Priya Ranjan Kumar <116024984+ranjanrana1712@users.noreply.github.com>
2023-11-27 22:48:39 +0530

    fix:remove slide

--- script.js
+++ script.js
@@ -195,21 +195,3 @@ function reverseStr(str) {
   }
   btn.addEventListener('click', clickHandler);
   
-  // const firstIndex =0;
-  // function automaticSlide(){
-  //   setTimeout(automaticSlide, 2000);
-  //   let pics;
-  //   const img = document.querySelectorAll('img');
-  //   for(pics=0; pics<img.length; pics++) {
-  //     img[pics].style.display = "none";
-
-
-  //   }
-  //   firstIndex++;
-  //   if(firstIndex>img.length){
-  //     firstIndex = 1;
-
-  //   }
-  //   img[firstIndex-1].style.display= "block";
-  // }
-  // automaticSlide();
